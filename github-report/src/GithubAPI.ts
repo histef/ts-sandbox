9e659726375fa5b83034301d70fdad80cc551ff7
Stefanie Chin <hi.stef.c@gmail.com>
2019-01-14 11:12:11 -0800

    refactor: so API returns json instead of me having to do that in the get request

--- github-report/src/GithubAPI.ts
+++ github-report/src/GithubAPI.ts
@@ -6,12 +6,13 @@ export default class GithubApi {
     const options: any = {
       headers: {
         "User-Agent": "request"
-      }
+      },
+      json: true
     }
 
     request.get(`https://api.github.com/users/${username}`, options, (error:any, response: any, body:any)=>{
       //takes response body and enters info into a new user instance
-      let user = new User(JSON.parse(body));
+      let user = new User(body);
       console.log(user);
     })
   }
