9ca6213f3bd4c67e64ac0d6ab97b21b43507dfeb
Stefanie Chin <hi.stef.c@gmail.com>
2019-01-14 12:48:00 -0800

    add callback to getUserInfo tohandle data received

--- github-report/src/GithubAPI.ts
+++ github-report/src/GithubAPI.ts
@@ -2,7 +2,8 @@ import * as request from 'request';
 import { User } from './User';
 
 export default class GithubApi {
-  getUserInfo(username: string) {
+  //takes 2 parameters username and a callback (cb). the callback takes 1 param user(with type User) and returns type any
+  getUserInfo(username: string, cb: (user: User) => any) {
     const options: any = {
       headers: {
         "User-Agent": "request"
@@ -13,7 +14,8 @@ export default class GithubApi {
     request.get(`https://api.github.com/users/${username}`, options, (error:any, response: any, body:any)=>{
       //takes response body and enters info into a new user instance
       let user = new User(body);
-      console.log(user);
+      // console.log(user); this logic now moved to index.ts that should handle what to do with logic
+      cb(user);
     })
   }
   getRepos() {
--- github-report/src/index.ts
+++ github-report/src/index.ts
@@ -1,5 +1,8 @@
 import GithubApi from './GithubAPI';
+import { User } from './User';
 
 //create a instance of the GithubAPI
 const callGithub = new GithubApi();
-callGithub.getUserInfo('histef');
+callGithub.getUserInfo('histef', (user) => {
+  console.log(user)
+});
