8ba91c1dc95fd0f11bee1c6dc750c883579802fe
Stefanie Chin <hi.stef.c@gmail.com>
2019-01-13 18:34:52 -0800

    create User and Repo file

---
+++ github-report/src/Repo.ts
@@ -0,0 +1,7 @@
+export default class Repo {
+  name: string;
+  url: string;
+  description: string;
+  size: number;
+  forkCount: number;
+}
---
+++ github-report/src/User.ts
@@ -0,0 +1,7 @@
+export class User {
+  login: string;
+  fullName: string;
+  repoCount: number;
+  followerCount: number;
+  repos: Repo[];
+}
