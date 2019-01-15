e292ba9f0e8f03653b420381216b8a634bc2ac57
Stefanie Chin <hi.stef.c@gmail.com>
2019-01-14 17:39:49 -0800

    make username dynamic

--- github-report/src/index.ts
+++ github-report/src/index.ts
@@ -4,8 +4,9 @@ import GithubApi from './GithubAPI';
 
 //create a instance of the GithubAPI
 const callGithub = new GithubApi();
-callGithub.getUserInfo('histef', (user) => {
-  callGithub.getRepos('histef', (repo) => {
+let username = process.argv[2];
+callGithub.getUserInfo(username, (user) => {
+  callGithub.getRepos(username, (repo) => {
     let top5Repos = _.take(repo, 5)
     user.repos = top5Repos;
     console.log(user);
