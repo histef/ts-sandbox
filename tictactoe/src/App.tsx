f48122df59a9b9d2ea9652484e294ac4c925166b
Stefanie Chin <hi.stef.c@gmail.com>
2019-01-16 18:08:35 -0800

    feat: each player can claim a cell

--- tictactoe/src/App.tsx
+++ tictactoe/src/App.tsx
@@ -7,42 +7,70 @@ const enum Player { None, One, Two }
 
 // the boards type is an array of Player's
 interface IState {
-  board: Player[]
+  board: Player[];
+  nextPlayerTurn: Player;
 }
 
-// first type is for props, second is for state
+// first type is for props(since we don't receive any, this is an empty object), second is for state
 class App extends React.Component<{}, IState> {
-public state = {
+  public state = {
     board: [
-            Player.None,
-            Player.None,
-            Player.None,
-            Player.None,
-            Player.None,
-            Player.None,
-            Player.None,
-            Player.None,
-            Player.None,
-          ]
+      Player.None,
+      Player.None,
+      Player.None,
+      Player.None,
+      Player.None,
+      Player.None,
+      Player.None,
+      Player.None,
+      Player.None,
+    ],
+    nextPlayerTurn: Player.One
   }
 
+  // event: React.MouseEvent<HTMLDivElement>
+  // index will tell which cell to update
+  // not allowed to do anon fn in tslint so need to 
+  public createOnClickHandler = (index: number) => () => {
+    const { board, nextPlayerTurn } = this.state;
+
+    // create new board for immutability of og board
+    const newBoard = board.slice();
+    newBoard[index] = nextPlayerTurn;
+
+    this.setState({
+      board: newBoard,
+      nextPlayerTurn: 3 - nextPlayerTurn
+    })
+  }
+  
+  // in order to prevent createOnClickHandler from executing right away, need to create anon fn (arrow function = lambdas) so that when it's clicked, then fn executes.
+  // BUT arrow fn(lambda) 'forbidden in jsx attributes'...so need to move it to createOnClickHandler function by returning an anon function
   public renderCell = (index: number) => {
-    return <div className="cell" key={Math.random()} />
+    const { board } = this.state;
+
+    // data-player gives every cell a player attribute
+    return <div className="cell"
+      key={Math.random()}
+      data-player={board[index]}
+      onClick={this.createOnClickHandler(index)}
+    />
   }
 
+  // map can take 3 args(current val, index, array), all we need is the 2nd param (index) but it needs to be the in correct order, so we have 1st param (value) even tho we never use it.
   public renderBoard = () => {
     const { board } = this.state;
     return <div className="board-container">
-    { board.map( (item)=>
-      this.renderCell(item)
-    )}
+      {board.map((value, index) =>
+        this.renderCell(index)
+      )}
     </div>
   }
 
   public render() {
     return (
       <div className="App">
-        <h1>Tac Tac Toe</h1>
+        <h1>Tic Tac Toe</h1>
         {this.renderBoard()}
       </div>
     );
